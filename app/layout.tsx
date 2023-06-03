8b59c2452aaee2706eb8f49f2bd59461b2807224
Abdul Rosyid <ocritocrit@gmail.com>
2023-06-03 12:47:30 +0700

    change meta data structure for use client component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
 import SocialIcon from '@/components/SocialIcon';
 import Navbar from '@/section/Navbar';
+import { useState } from 'react';
 import { Fira_Code, Raleway } from 'next/font/google';
 import '../scss/index.scss';
 import ScrollToTopButton from '@/components/ScrollToTopButton';
+// export const metadata = {
+//   title: 'ocitocit | A passionate frontend developer from Indonesia',
+//   description: 'A passionate frontend developer from Indonesia',
+//   verification: {
+//     google: 'JZ25rAK1bmrTlhyIZdIylQxObcro3f65HbLGBJiY_Hc'
+//   }
+// };
 
-export const metadata = {
-  title: 'ocitocit | A passionate frontend developer from Indonesia',
-  description: 'A passionate frontend developer from Indonesia',
-  verification: {
-    google: 'JZ25rAK1bmrTlhyIZdIylQxObcro3f65HbLGBJiY_Hc'
-  }
-};
 const raleway = Raleway({
   subsets: ['latin'],
   variable: '--raleway'
@@ -25,6 +26,15 @@ const firacode = Fira_Code({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <title>ocitocit | A passionate frontend developer from Indonesia</title>
+        <meta name="description" content="A passionate frontend developer from Indonesia" />
+        <meta
+          name="google-site-verification"
+          content="JZ25rAK1bmrTlhyIZdIylQxObcro3f65HbLGBJiY_Hc"
+        />
+      </head>
+
       <body className={`app ${raleway.variable} ${firacode.variable}`}>
         <>
           <Navbar />
